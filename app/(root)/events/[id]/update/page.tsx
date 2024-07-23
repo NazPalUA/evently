ecb181ebd348114404d7075560756459e487d68b
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-07-23 18:58:56 +0300

    refactor: Add UpdateEvent page for event modification

---
+++ app/(root)/events/[id]/update/page.tsx
@@ -0,0 +1,40 @@
+import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
+import { auth } from "@clerk/nextjs/server";
+
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+  // Get the userId from auth() -- if null, the user is not signed in
+  const { userId } = auth();
+  const event = await getEventById(id);
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Update Event
+        </h3>
+      </section>
+
+      <div className="wrapper my-8">
+        {userId ? (
+          <EventForm
+            type="Update"
+            event={event}
+            eventId={event._id}
+            userId={userId}
+          />
+        ) : (
+          <p>Sign in to update an event</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default UpdateEvent;
